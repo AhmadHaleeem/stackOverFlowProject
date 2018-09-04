06ac2c634d1a5670f7754faa3cb8311e3aa3874a
AhmadHaleeem <coldman2099@gmail.com>
2018-09-04 23:17:04 +0200

    Ajaxify the delete button

--- resources/assets/js/components/Answer.vue
+++ resources/assets/js/components/Answer.vue
@@ -23,7 +23,7 @@
               this.editing = false
             },
             update() {
-                axios.patch(`/questions/${this.questionId}/answers/${this.id}`, {
+                axios.patch(this.endpoint, {
                     body: this.body
                 })
                 .then(res => {
@@ -34,12 +34,25 @@
                 .catch(err => {
                     alert(err.response.data.message)
                 })
+            },
+            destroy() {
+                if (confirm ('Are you sure?')) {
+                    axios.delete(this.endpoint)
+                        .then(res => {
+                            $(this.$el).fadeOut(500, () => {
+                                alert(res.data.message)
+                            })
+                    })
+                }
             }
         },
         computed: {
             isInvalid() {
                 // if the the body value is less than 10 chars then it will return true otherwise it will return false
                 return this.body.length < 10
+            },
+            endpoint() {
+                return `/questions/${this.questionId}/answers/${this.id}`;
             }
         }
     }
